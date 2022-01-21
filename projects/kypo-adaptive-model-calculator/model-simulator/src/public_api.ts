7a2bf081f2eaefea05b93f4703143cb0cd83dd98
244021 <244021@mail.muni.cz>
2022-01-21 11:33:13 +0100

    Rename calculator to simulator

--- projects/kypo-adaptive-model-calculator/model-simulator/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of @muni-kypo-crp/adaptive-model-calculator/model-simulator
- */
-
-export * from './model-simulator.component';
-export * from './model-simulator.module';
-export * from './model/trainee-phase-statistics';
---
+++ projects/kypo-adaptive-model-simulator/model-simulator/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of @muni-kypo-crp/adaptive-model-simulator/model-simulator
+ */
+
+export * from './model-simulator.component';
+export * from './model-simulator.module';
+export * from './model/trainee-phase-statistics';
